5ffca1a2964ec6b8c29f20e5d30787bbf0ebd4af
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-01-22 18:56:47 +0200

    Add files via upload

---
+++ textanim.js
@@ -0,0 +1,32 @@
+let text = ["apollo 8", "hello", "bye"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+let rand = getRandomInt(3);
+function add_text(id, text) {
+  var elem = document.getElementById(id);
+  var pos = 0;
+  function add_letter() {
+    pos++;
+    elem.innerHTML = text.slice(0, pos);
+
+    if (pos > text.length) {
+      clearInterval(timer);
+    }
+  }
+  let timer = setInterval(add_letter, 1000);
+}
+
+function del_text(id, text) {
+  var elem = document.getElementById(id);
+  var pos = 0;
+  function del_letter() {
+    pos--;
+    elem.innerHTML = text.slice(0, pos);
+
+    if (pos == 0) {
+      clearInterval(rtimer);
+    }
+  }
+  let rtimer = setInterval(del_letter, 1000);
+}
