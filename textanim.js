0ef9e620018b02d82c414bd9708b72acdf283970
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-01-22 20:03:31 +0200

    Update textanim.js

--- textanim.js
+++ textanim.js
@@ -1,32 +1,32 @@
-let text = ["apollo 8", "hello", "bye"];
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-let rand = getRandomInt(3);
-function add_text(id, text) {
-  var elem = document.getElementById(id);
-  var pos = 0;
-  function add_letter() {
-    pos++;
-    elem.innerHTML = text.slice(0, pos);
+var arr_src = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROatvj44LZ_o8-uyc1QxmaZfLEkpLXKnuMvjzMEpL8VOVxDgcF_gjOwvH4KdvOoWv4Ws4&usqp=CAU",
+  " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKY5HHngoypOTRj4KZ7zAu-qXs9kzXQFB7td1Hp82nXR8PxWQ9YRiAF4y3YyS9aDwPIrI&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLVixRcB3ogSgQTolsw0n71gC1Pg3Fod1r_eC_HCRQtldqcLDBZcMlLBL96YsV0Zihs1U&usqp=CAU",
+];
+var i = 0;
 
-    if (pos > text.length) {
-      clearInterval(timer);
-    }
+var img1 = document.getElementById("img1");
+
+function next() {
+  if (i < arr_src.length - 1) {
+    i++;
+  } else {
+    i = 0;
   }
-  let timer = setInterval(add_letter, 1000);
+  img1.setAttribute("src", arr_src[i]);
 }
 
-function del_text(id, text) {
-  var elem = document.getElementById(id);
-  var pos = 0;
-  function del_letter() {
-    pos--;
-    elem.innerHTML = text.slice(0, pos);
-
-    if (pos == 0) {
-      clearInterval(rtimer);
-    }
+function prev() {
+  if (i > 0) {
+    i--;
+  } else {
+    i = 2;
   }
-  let rtimer = setInterval(del_letter, 1000);
+  img1.setAttribute("src", arr_src[i]);
 }
+let timerId;
+let stop;
+// let timerId = setInterval(function () {
+//   next();
+// }, 100);
+// let stop = clearInterval(timerId);
